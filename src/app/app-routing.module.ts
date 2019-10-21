2ddfd7bdb9264f718dc1babb2b28c34d7f83300d
raskardev <alexcdev95@gmail.com>
2019-10-21 08:05:15 +0200

    feat(): add basic html to app component and some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostsListComponent,
+  },
+  {
+    path: 'posts/:id',
+    component: PostDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
