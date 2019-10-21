5f555c66944213d556f664e29cf71b2d47f7ca02
raskardev <alexcdev95@gmail.com>
2019-10-21 08:04:13 +0200

    feat(): add HttpClientModule, services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostService } from './services/post.service';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PostsListComponent, PostDetailComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [PostService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
